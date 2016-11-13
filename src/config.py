b56b16a44000d4f526420bccfd49858cc296768b
FreakJoe <sandmensch933@web.de>
2016-11-13 11:28:09 +0100

    Set up config

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import configparser
+
+class Config():
+	def __init__(self):
+		self.config = configparser.ConfigParser()
+		self.config.read("..\config.ini")
+
+		self.values = {}
+		self.read_values()
+
+	def read_values(self):
+		if 'db' in self.config.sections():
+			for option in self.config.options('db'):
+				self.values[option] = self.config.get('db', option)
+
+		else:
+			self.values = None
